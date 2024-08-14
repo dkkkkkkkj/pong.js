af5b7fde44807cdb9c6691628448c8e9c68091ce
dkkkkkkkj <00001081308047sp@al.educacao.sp.gov.br>
2024-08-14 11:49:31 -0300

    pong.js

---
+++ sketch.js
@@ -0,0 +1,28 @@
+//variáveis da bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let diâmetro = 20;
+let raio = diâmetro / 2;
+
+//velocidade da bolinha
+let velocidadeXbolinha = 6;
+let velocidadeYbolinha = 6;
+
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let RaqueteComprimento = 10;
+let RaqueteAltura = 90;
+
+//variáveis do oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+let velocidadeYOponente;
+
+let colidiu = false;
+
+//placar do jogo
+let meusPontos = 0;
+let pontosDoOponente = 0;
+
+//sons do jogo
